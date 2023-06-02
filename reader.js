6837954e3f752e8189a70fa5bb31db54c6f10409
nwnisworking <66354122+nwnisworking@users.noreply.github.com>
2023-06-02 21:52:58 +0800

    Update reader.js

--- reader.js
+++ reader.js
@@ -1,8 +1,7 @@
 /**
  * MP4 reader 
  * 
- * Read the atom structure of MP4 and return all known atoms in a form of array. 
- * It also records the start and end position so that user can extrapolate the data.
+ * Read the atom structure of MP4 and return all known atom positions in a form of array. 
  */
 
 /**
@@ -10,33 +9,30 @@
  * @param {number} i Cursor for finding the current offset 
  * @param {Array} children All the atoms in an Array Object
  */
-function reader(file, i = 0, children = []){
-  const data = {children : []}
-  data['size'] = file.getUint32(i)
-  data['name'] = String.fromCharCode(...new Uint8Array(file.buffer.slice(i + 4, i + 8)))
-  data['start'] = i
-  data['end'] = i + data['size']
-  
-  switch(name){
-    // Atom contains additional 4 byte which returns the length of entries
-    case 'dref' : case 'stsd' : i+= 4 
-    // Atom contains version and flag 
-    case 'meta' : i+= 4 
-    case 'moov' : 
-    case 'trak' : 
-    case 'edts' : 
-    case 'mdia' : 
-    case 'dinf' : 
-    case 'stbl' : 
-    case 'udta' : 
-    case 'ilst' : 
-    case 'minf' : 
-      i+= 8 // Normal container atom
-    break
-    default :
-      i+= size 
-  }
-  
-  children.push(data)
-  return i < file.byteLength ? reader(file, i, children) : children
-}
+export function reader(file, i = 0, children = []){
+		const size = file.getUint32(i), 
+		name = String.fromCharCode(...new Uint8Array(file.buffer.slice(i + 4, i + 8))),
+		y = {name, size, start : i, end : i + size}
+	
+		switch(name){
+			case 'dref' : case 'stsd' : i+= 4 // Atom contains additional 4 byte which returns the length of entries
+			case 'meta' : i+= 4 // Atom contains version and flag 
+			case 'moov' : 
+			case 'trak' : 
+			case 'edts' : 
+			case 'mdia' : 
+			case 'dinf' : 
+			case 'stbl' : 
+			case 'udta' : 
+			case 'ilst' : 
+			case 'minf' : 
+				i+= 8
+			break
+			default :
+				i+= size
+		}
+	
+		children.push(y)
+
+		return i < file.byteLength ? reader(file, i, children) : children
+	}
